9e46332d8c0d3094f34db3bb529f7215f70c3375
lokeshbs1994 <lokeshbs619@gmail.com>
2022-01-30 13:14:53 +0530

    batch 107 proj

---
+++ src/main/java/com/bridgelabz/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Enter two numbers...");
+        Scanner sc = new Scanner(System.in);
+// int i1 = sc.nextInt();
+        System.out.println("Your First input : " + sc.nextInt());
+// int i2 = sc.nextInt();
+        System.out.println("Your Second input : " + sc.nextInt());
+// HelloWorld.multiply(i1, i2);
+    }
+}
---
+++ src/main/java/com/bridgelabz/dayone/HelloWorld.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.dayone;
+
+import java.util.Scanner;
+
+public class HelloWorld{
+}
